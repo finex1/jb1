28049305c49a72af7808f7ddc5af37312f3381b0
finex1 <finex@youths4youths.com>
2018-11-29 17:24:17 +0000

    Add files via upload

--- routes/activity.js
+++ routes/activity.js
@@ -233,7 +233,7 @@ exports.execute = function (req, res) {
 			var url ='http://requestbin.fullcontact.com/10sa3c91'
 			request({url:url,
 					method:"POST",
-					json:accessTokenRequest
+					json:payload
 					}, function (error, response, body) {
 			  if (!error) {
 				console.log(body);
